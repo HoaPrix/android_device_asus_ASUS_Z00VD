0b10add2de97b243305e922e9da28f45022f71ab
HoaPrix <lebuidihoa257@gmail.com>
2017-09-21 13:24:52 +0700

    cm-13.0
    Change-Id: Ie03eb54c6d520a80f46270874563ed6aa01dcf64

--- cm.mk
+++ cm.mk
@@ -12,5 +12,11 @@ $(call inherit-product, device/asus/Z00VD/full_Z00VD.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+PRODUCT_NAME := cm_Z00VD
+PRODUCT_DEVICE := Z00VD
+PRODUCT_BRAND := asus
+PRODUCT_MANUFACTURER := Asus
+PRODUCT_MODEL := Asus Z00VD
+
 # Google playstore
 PRODUCT_GMS_CLIENTID_BASE := android-one
--- full_Z00VD.mk
+++ full_Z00VD.mk
@@ -3,7 +3,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from fireball device
-$(call inherit-product, device/asus/Z00VD/device_mt6580.mk)
+$(call inherit-product, device/asus/Z00VD/device_Z00VD.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_BRAND := Asus
